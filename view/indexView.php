feacee4768a7df88db01dad958c1feed4677284d
Nichita Maftei <nichitamaftei04@gmail.com>
2025-01-09 20:26:18 +0000

    Laying the foundation to edit call fows
    Added a view and controller with some logic for editing call flows

---
+++ view/indexView.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../view/css_js_images/style.css"> <!-- linking to style sheet -->
+        <script src="../@joint/core/dist/joint.js"></script> <!-- linking to JointJS library -->
+        <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script> <!-- linking to j query -->
+        <script type="text/javascript" src="../view/css_js_images/javascript.js"></script> <!-- linking to javascript -->
+        <title>Index</title>
+    </head>
+
+    <body id="indexView">
+        <!-- top bar of the screen -->
+        <section id='top-bar'>
+
+            <div id="hamburger-container">
+                <button class="hamburger" onclick="showMenu()">
+                    <img id="menu-icon"src="../view/css_js_images/burgerMenuIcon.png" alt="">
+                </button>
+            </div>
+
+            <h1><?= $departmentName ?> </h1>
+        
+            <div id="side-menu">
+                <ul id="unordered-list">
+                    
+                    <li class="list-items">
+                        <button class="backButton" onclick="hideMenu()">
+                            <img id="backbutton-icon"src="../view/css_js_images/backButtonIcon.png" alt="">
+                        </button>
+                    </li>
+
+                    <?php foreach ($arrayOfDepartments as $index => $department): ?>   <!-- for every department, create links -->
+                        <form method="POST" action="index.php">
+                            <li class="list-items">
+                                <button type="submit" name="dept" value="<?= $index; ?>" class="departmentButtons">
+                                    <?= $department['name']; ?>
+                                </button>
+                            </li>
+                        </form>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        </section>        
+
+        <div id="smallCanvas" onclick="fullView()"></div>
+
+        <div id="canvas-modal"> <!-- initially hidden -->
+            <div id="bigCanvas">
+            </div>
+            <button id="close-modal" onclick="closeBigCanvas()">Close</button>
+        </div>
+
+        <div>   
+            <a href="../controller/editCallFlow.php"><button>Edit Call Flow</button></a>
+        </div>
+    </body>
+
+</html>
